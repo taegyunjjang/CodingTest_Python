cb4b3b23c4d59ecef8c6d000bed3afb8a1740dd4
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-07-22 19:58:15 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 3.80 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,6 +1,2 @@
 def solution(myString):
-    answer = []
-    for s in myString.split('x'):
-        if s != '':
-            answer.append(s)
-    return sorted(answer)
+    return sorted([s for s in myString.split('x') if s != ''])
