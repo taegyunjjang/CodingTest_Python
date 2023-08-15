42986f703eda8a4d603a05bd74e78a78325fbdc8
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-08-15 11:28:27 +0900

    [level 2] Title: 주식가격, Time: 57.12 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -4,9 +4,9 @@ def solution(prices):
     isDescend = False
     
     for i in range(l):
-        if i == l - 1:
-            answer.append(0)
-            break
+        # if i == l - 1:
+        #     answer.append(0)
+        #     break
         for j in range(i + 1, l):
             if prices[i] > prices[j]:
                 answer.append(j - i)
