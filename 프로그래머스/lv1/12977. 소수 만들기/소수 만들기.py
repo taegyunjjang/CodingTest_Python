e17786626c70736189ab4625b60033cb7ce22835
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-08-18 18:47:44 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for comb in list(combinations(nums, 3)):
+        if isPrime(sum(comb)):
+            answer += 1
+    return answer
+
+def isPrime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
