40db841f70ee267b75f99c2a28861d3fcf782ad2
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-08-24 01:08:23 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,16 @@
+def solution(N, stages):
+    answer = []
+    failureRate = [0 for _ in range(N)]
+    d = len(stages)
+    
+    for i in range(N):
+        failureRate[i] = (stages.count(i + 1) / d, i + 1)
+        d -= stages.count(i + 1)
+        if d == 0:
+            for j in range(i + 1, N):
+                failureRate[j] = (0, j + 1)
+            break
+    failureRate.sort(reverse=True, key=lambda x: x[0])
+    for j in range(N):
+        answer.append(failureRate[j][1])
+    return answer
