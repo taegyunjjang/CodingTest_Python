0f5ed363a46542ae77595854d1f1dd3728ccfdfa
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-09-05 16:05:10 +0900

    [level 2] Title: 숫자의 표현, Time: 0.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,28 +1,9 @@
 def solution(n):
-    cnt = 0
-    if n % 2 == 0 or n == 1:
-        cnt = 1
-    else:
-        cnt = 2
-    for i in range(3, n // 2 + 1, 2):
-        if n % i == 0:
-            cnt += 1
-    for i in range(4, n, 2):
-        rest = n % i
-        if n // 2 > rest:
-            break
-        elif rest == i // 2:
-            cnt += 1
-    return cnt
-    # cnt = 0
-    # nList = [i for i in range(1, n + 1)]
-    # for i in range(n - 1):
-    #     for j in range(1, n - i - 1):
-    #         if sum(nList[i:i + j]) == n:
-    #             cnt += 1
-    # return cnt
+    answer = 0
 
-    # [1,2,3,4,5]
-    # 36
-    # 1, 2, 3, 4, 6, 9, 12, 18, 36
-    # 36, 111213 78910
+    for i in range(1, n + 1):
+        if n % i == 0:
+            if i % 2 == 1:
+                answer += 1
+
+    return answer
