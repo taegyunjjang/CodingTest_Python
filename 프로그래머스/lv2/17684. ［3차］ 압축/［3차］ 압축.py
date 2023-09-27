a67abf1889a89c083c67ceebb1a3be4f7670de0c
taegyunjjang <90623615+taegyunjjang@users.noreply.github.com>
2023-09-27 19:52:45 +0900

    [level 2] Title: [3차] 압축, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -5,37 +5,46 @@ def solution(msg):
 
     while i < len(msg):
         tmp = msg[i]
+        
+        # 마지막 idx라면 idxList에 넣고 종료
         if i == len(msg) - 1:
             idxList.append(w[tmp])
             break
-
+        
+        # 현재 문자 + 다음 문자가 딕셔너리에 있는 경우
         if msg[i] + msg[i + 1] in w.keys():
             tmp += msg[i + 1]
             i += 1
-
+            
+            # 다음 idx가 마지막 idx라면 idxList에 넣고 종료
             if i == len(msg) - 1:
                 idxList.append(w[tmp])
                 break
-
+            
+            # 딕셔너리에 존재하는 가장 긴 문자열 찾기
             while isInW(w, tmp, i, msg):
                 tmp += msg[i + 1]
                 i += 1
+                
+                # 가장 긴 문자열에 해당하는 idx가 마지막 idx라면 idxList에 넣고 종료
                 if i == len(msg) - 1:
                     idxList.append(w[tmp])
                     return idxList
-
+                
             w[tmp + msg[i + 1]] = len(w) + 1
             idxList.append(w[tmp])
+        
+        # 현재 문자 + 다음 문자가 딕셔너리에 없는 경우
         else:
             idxList.append(w[tmp])
             tmp += msg[i + 1]
             w[tmp] = len(w) + 1
         
         i += 1
-
     return idxList
 
-def isInW(w, subStr, i, msg):
-    if subStr + msg[i + 1] in w.keys():
+# 현재 문자 + 다음 문자가 딕셔너리에 존재하는지에 대한 함수
+def isInW(w, tmp, i, msg):
+    if tmp + msg[i + 1] in w.keys():
         return True
     return False
