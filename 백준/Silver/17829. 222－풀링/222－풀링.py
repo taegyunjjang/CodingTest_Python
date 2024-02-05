21480f08850dd043c3d5fb76f9bc2d2dc6c67a8f
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-02-06 01:08:34 +0900

    [Silver II] Title: 222-풀링, Time: 792 ms, Memory: 75804 KB -BaekjoonHub

---
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -0,0 +1,23 @@
+import sys
+
+def pooling(ary, tmp, n):
+    tmp = []
+    if n == 2:
+        ary = ary[0] + ary[1]
+        return sorted(ary)[2]
+    else:
+        tmp.append(pooling([row[:n//2] for row in ary[:n//2]], tmp, n//2))
+        tmp.append(pooling([row[n//2:] for row in ary[:n//2]], tmp, n//2))
+        tmp.append(pooling([row[:n//2] for row in ary[n//2:]], tmp, n//2))
+        tmp.append(pooling([row[n//2:] for row in ary[n//2:]], tmp, n//2))
+    
+    return sorted(tmp)[2]
+
+input = sys.stdin.readline
+N = int(input())
+ary = []
+tmp = []
+for _ in range(N):
+    ary.append(list(map(int, input().split())))
+
+print(pooling(ary, tmp, N))
