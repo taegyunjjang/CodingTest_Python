495c679cd391f68551f681efd98a53da6caf1eb7
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-02-19 12:53:56 +0900

    [Silver III] Title: 이전 순열, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,28 @@
+import sys
+
+
+input = sys.stdin.readline
+N = int(input())
+num_list = list(map(int, input().split()))
+reversed_list = num_list[::-1]
+loops = len(num_list)
+
+answer = []
+for i in range(loops - 1):
+    if reversed_list[i] < reversed_list[i + 1]:
+        concat_list = reversed_list[:i + 1]
+        tmp, idx = 0, 0
+        for j in range(len(concat_list)):
+            if reversed_list[j] < reversed_list[i + 1]:
+                if tmp < reversed_list[j]:
+                    tmp = reversed_list[j]
+                    idx = j
+        concat_list[idx] = reversed_list[i + 1]
+        concat_list.sort(reverse=True)
+        answer = num_list[:N - i - 2] + [tmp] + concat_list
+        break
+
+if answer:
+    print(*answer)
+else:
+    print(-1)
