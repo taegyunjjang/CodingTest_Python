dea9e2728453e266e7b65a2c655e0c06cff3f647
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-02-25 19:33:47 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 5812 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.py
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.py
@@ -20,24 +20,17 @@ def solution():
     # 주어진 수가 소수가 아닐 때
     if not is_prime(int(N)):
         print("no")
-# 1000000000000000
+
     # 주어진 수가 소수일 때
     else:
         if '3' in N or '4' in N or '7' in N:
             print("no")
         else:
-            tmp = ''
-            for num in N:
-                if num == '6':
-                    tmp = '9' + tmp
-                elif num == '9':
-                    tmp = '6' + tmp
-                else:
-                    tmp = num + tmp
-
-            N = int(tmp)
+            N = N[::-1].replace('6', 'a').replace('9', '6').replace('a', '9')
+            N = int(N)
             if is_prime(N):
                 print("yes")
             else:
                 print("no")
+                
 solution()
