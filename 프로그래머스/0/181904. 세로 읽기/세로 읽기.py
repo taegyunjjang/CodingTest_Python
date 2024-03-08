6fc62beefae3893dfa4b02794980796532e35a6b
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-08 17:07:16 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,8 +1,2 @@
-def solution(st, m, c):
-    temp = []
-    answer = ''
-    x = int(len(st)/m)
-    for i in range(x):
-        temp.append(st[i*m:(i+1)*m])
-        answer += temp[i][c-1]
-    return answer
+def solution(s, m, c):
+    return s[c-1::m]
