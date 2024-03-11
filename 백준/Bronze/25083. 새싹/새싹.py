b70a010082e707b20ad780bd2b9d918a1d847179
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-11 16:18:17 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    print(f'         ,r\'\"7')
+    print(f'r`-_   ,\'  ,/')
+    print(f' \. \". L_r\'')
+    print(f'   `~\/')
+    print(f'      |')
+    print(f'      |')
+solution()
+
+'''
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+'''
