f8a0967f5b85748e0bc0456f553a1bbd12741eed
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-20 20:04:30 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << 147 << endl;
+    cout << "tgkim12" << endl;
+}
