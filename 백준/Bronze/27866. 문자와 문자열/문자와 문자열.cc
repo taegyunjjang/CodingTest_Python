280606d2ac297dcd999281f344f190aa5a49741b
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-27 16:47:49 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string str;
+    cin >> str;
+    int i;
+    cin >> i;
+    cout << str[i - 1] << endl;
+}
