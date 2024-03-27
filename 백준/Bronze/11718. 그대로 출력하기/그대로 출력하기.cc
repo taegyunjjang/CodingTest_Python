71b8c8a4b16d030896711fb40f0316736b16dd61
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-27 17:04:13 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string str;
+    
+    int i = 0;
+    while (i < 100){ 
+        getline(cin, str);        
+        cout << str << endl;
+        i += 1;
+    }
+}
