6c4b156677d64f506114f9904ba6b0d8ddae4260
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-27 12:03:13 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,18 +1,19 @@
+#include <bits/stdc++.h>
 #include <iostream>
+
+#define endl '\n'
 using namespace std;
 
 int main()
 {
-    int A, B;
-    
-    while (true)
-    {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    while (true) {
+        int A, B;
         cin >> A >> B;
         if (A == 0 && B == 0)
-        {
             break;
-        }
         cout << A + B << endl;
     }
-    return 0;
 }
