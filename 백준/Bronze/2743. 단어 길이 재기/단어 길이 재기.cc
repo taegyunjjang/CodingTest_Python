a3f9962d1279c8b77a595665c8686df16b18ff43
김태균 <90623615+taegyunjjang@users.noreply.github.com>
2024-03-27 16:29:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    cin >> S;
+    cout << S.length() << endl;
+}
